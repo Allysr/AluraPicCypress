8f608935bca13ec509f455e84c33a45bfd2d0d9d
Alicia <alicia.rsouza@gmail.com>
2023-01-22 11:27:56 -0300

    feat: Metodo Post

--- cypress/e2e/AluraPic/AluraPicAPI.cy.js
+++ cypress/e2e/AluraPic/AluraPicAPI.cy.js
@@ -1,6 +1,6 @@
 describe("Buscar fotos e dados", () => {
   it("Buscar fotos do Flavio", () => {
-    cy.request({ 
+    cy.request({
       method: "GET",
       url: "https://apialurapic.herokuapp.com/flavio/photos",
     }).then((res) => {
@@ -8,6 +8,19 @@ describe("Buscar fotos e dados", () => {
       expect(res.body).is.not.empty;
       expect(res.body[0]).to.have.property("description");
       expect(res.body[0].description).to.be.equal("Farol iluminado");
+    });
+  });
+
+  it("Fazer login do Flavio", () => {
+    cy.request({
+      method: "POST",
+      url: "https://apialurapic.herokuapp.com/user/login",
+      body: Cypress.env(),
+    }).then((res) => {
+      expect(res.status).to.be.equal(200);
+      expect(res.body).is.not.empty;
+      expect(res.body).to.have.property("id");
+      expect(res.body.id).to.be.equal(1);
     });
   });
 });
