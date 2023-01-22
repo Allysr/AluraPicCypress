ba5571615b34fba4c8c0d655657180221e0a3af9
Alicia <alicia.rsouza@gmail.com>
2023-01-22 13:03:07 -0300

    feat: Adição da rota com erro 400

--- cypress/e2e/AluraPic/aluraBuscaUI.cy.js
+++ cypress/e2e/AluraPic/aluraBuscaUI.cy.js
@@ -1,6 +1,9 @@
 describe("Alura Fotos ", () => {
   beforeEach(() => {
     cy.visit("/#/home");
+    cy.intercept('POST',"/#/home/user/login", {
+      statusCode: 400
+    } ). as ('stubPost')
   });
 
   it("Ao clicar no botão de registrar sem dados, deve ocorrer um erro.", () => {
@@ -15,25 +18,19 @@ describe("Alura Fotos ", () => {
 
   it("Verifica se o email está invalido.", () => {
     cy.contains("a", "Register now").click();
-    cy.get('input[formcontrolname="email"]').type("alicia");
+    cy.get('input[formcontrolname="email"]').type('alicia');
     cy.contains("button", "Register").click();
     cy.contains("ap-vmessage", "Invalid e-mail").should("be.visible");
   });
 
-  it("Verifica se o email está invalido.", () => {
-    cy.contains("a", "Register now").click();
-    cy.get('input[formcontrolname="password"]').type(Cypress.env(password));
-    cy.contains("button", "Register").click();
-    cy.contains("ap-vmessage", "Mininum length is 8").should("be.visible");
-  });
-
-  it("Fazer login de usuario válido", () => {
-    cy.login(Cypress.env(userName), Cypress.env(password));
+  it.only("Fazer login de usuario válido", () => {
+    cy.login(Cypress.env("username"), Cypress.env("password"));
+    cy.wait('@stubPost')
     cy.contains("a", "flavio");
   });
 
-  it("Fazer login de usuario Inválido", () => {
-    cy.login(Cypress.env(userName), Cypress.env(password));
+  it.only("Fazer login de usuario Inválido", () => {
+  cy.login("teste", "senhaerrada");
     cy.on("window:alert", (str) => {
       expect(str).to.equal("Invalid user name or password");
     });
@@ -41,7 +38,7 @@ describe("Alura Fotos ", () => {
 
   const usuarios = require("../../fixtures/usuarios.json");
   usuarios.forEach((usuario) => {
-    it.only(`Registrar novo usuário ${usuario.userName}`, () => {
+    it(`Registrar novo usuário ${usuario.userName}`, () => {
       cy.contains("a", "Register now").click();
       cy.contains("button", "Register").click();
       cy.get('input[formcontrolname="email"]').type(usuario.email);
